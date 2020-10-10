ed477402208680a622a81d4db477b0ef4c30ace3
Prajita <prajitacst@gmail.com>
2020-10-10 15:34:44 +0530

    öne test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import LogoutContainer from './Logout';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+
+test('renders without crashing', () => {
+  const { getByText } = render(<LogoutContainer />);
+  const linkElement = getByText(/Thank you/i);
   expect(linkElement).toBeInTheDocument();
+
 });
